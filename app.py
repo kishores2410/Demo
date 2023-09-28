6791955278455a1d2954e79ad06778f5a754e099
Kishore S <111968897+kishores2410@users.noreply.github.com>
2023-09-28 14:24:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,242 +1,219 @@
-import os
-import tempfile
-import datetime as dt
-import whisper
-from pytube import YouTube
+import pandas as pd
+import numpy as np
 import streamlit as st
+import whisper
+import pytube
+from pytube import YouTube
+from streamlit_chat import message
+import openai
+from openai.embeddings_utils import get_embedding, distances_from_embeddings
+import os
+import pinecone
+from dotenv import load_dotenv
 
-# Initialize variables
-chat_history = []
-result = None
-chain = None
-run_once_flag = False
-call_to_load_video = 0
+# whisper
+model = whisper.load_model('base')
+output = ''
+data = []
+data_transcription = []
+embeddings = []
+mp4_video = ''
+audio_file = ''
 
-# Set Streamlit page config
-st.set_page_config(layout="wide")
-st.title("Video Transcription and Chatbot")
+# Pinacone
 
-# Function to process video and return transcribed text
-def process_video(video=None, url=None):
-    global result
+# Uncomment this section if you want to save the embedding in pinecone
+#load_dotenv()
+# initialize connection to pinecone (get API key at app.pinecone.io)
+# pinecone.init(
+#     api_key=os.getenv("PINACONE_API_KEY"),
+#     environment=os.getenv("PINACONE_ENVIRONMENT")
+# )
+array = []
 
-    if url:
-        file_dir = load_video(url)
-    else:
-        file_dir = video
+# Uncomment this section if you want to upload your own video
+# Sidebar
+# with st.sidebar:
+#     user_secret = st.text_input(label = ":blue[OpenAI API key]",
+#                                 value="",
+#                                 placeholder = "Paste your openAI API key, sk-",
+#                                 type = "password")
+#     youtube_link = st.text_input(label = ":red[Youtube link]",
+#                                 value="https://youtu.be/rQeXGvFAJDQ",
+#                                 placeholder = "")
+#     if youtube_link and user_secret:
+#         youtube_video = YouTube(youtube_link)
+#         video_id = pytube.extract.video_id(youtube_link)
+#         streams = youtube_video.streams.filter(only_audio=True)
+#         stream = streams.first()
+#         if st.button("Start Analysis"):
+#             if os.path.exists("word_embeddings.csv"):
+#                 os.remove("word_embeddings.csv")
+                
+#             with st.spinner('Running process...'):
+#                 # Get the video mp4
+#                 mp4_video = stream.download(filename='youtube_video.mp4')
+#                 audio_file = open(mp4_video, 'rb')
+#                 st.write(youtube_video.title)
+#                 st.video(youtube_link) 
 
-    st.write('Transcribing Video with Whisper base model')
-    model = whisper.load_model("base")
-    result = model.transcribe(file_dir)
+#                 # Whisper
+#                 output = model.transcribe("youtube_video.mp4")
+                
+#                 # Transcription
+#                 transcription = {
+#                     "title": youtube_video.title.strip(),
+#                     "transcription": output['text']
+#                 }
+#                 data_transcription.append(transcription)
+#                 pd.DataFrame(data_transcription).to_csv('transcription.csv') 
+#                 segments = output['segments']
 
-# Function to load video from YouTube and return the file path
-def load_video(url:str) -> str:
-    global result
+#                 # Pinacone index
+#                 # check if index_name index already exists (only create index if not)
+#                 # index_name = str(video_id)
+#                 # # check if 'index_name' index already exists (only create index if not)
+#                 # if 'index1' not in pinecone.list_indexes():
+#                 #     pinecone.create_index('index1', dimension=len(segments))
+#                 # # connect to index
+#                 # index = pinecone.Index('index1')
+                
+#                 #st.write(segments)
+#                 #Embeddings
+#                 for segment in segments:
+#                     openai.api_key = user_secret
+#                     response = openai.Embedding.create(
+#                         input= segment["text"].strip(),
+#                         model="text-embedding-ada-002"
+#                     )
+#                     embeddings = response['data'][0]['embedding']
+#                     meta = {
+#                         "text": segment["text"].strip(),
+#                         "start": segment['start'],
+#                         "end": segment['end'],
+#                         "embedding": embeddings
+#                     }
+#                     data.append(meta)
+#                 # upsert_response = index.upsert(
+#                 #         vectors=data,
+#                 #         namespace=video_id
+#                 #     )
+#                 pd.DataFrame(data).to_csv('word_embeddings.csv') 
+#                 os.remove("youtube_video.mp4")
+#                 st.success('Analysis completed')
 
-    yt = YouTube(url)
-    target_dir = os.path.join('/tmp', 'Youtube')
-    if not os.path.exists(target_dir):
-        os.mkdir(target_dir)
+st.markdown('<h1>Youtube GPT 🤖<small> by <a href="https://codegpt.co">Code GPT</a></small></h1>', unsafe_allow_html=True)
+st.write("Start a chat with this video of Microsoft CEO Satya Nadella's interview. You just need to add your OpenAI API Key and paste it in the 'Chat with the video' tab.")
 
-    if os.path.exists(target_dir+'/'+yt.title+'.mp4'):
-        return target_dir+'/'+yt.title+'.mp4'
-    try:
+DEFAULT_WIDTH = 80
+VIDEO_DATA = "https://youtu.be/bsFXgfbj8Bc"
 
-        yt.streams.filter(only_audio=True)
-        stream = yt.streams.get_audio_only()
-        st.write('----DOWNLOADING AUDIO FILE----')
-        stream.download(output_path=target_dir)
-    except:
-        st.error('Issue in Downloading video')
+width = 40
 
-    return target_dir+'/'+yt.title+'.mp4'
+width = max(width, 0.01)
+side = max((100 - width) / 2, 0.01)
 
-# Function to process video and return transcribed text
-def process_text(video=None, url=None):
-    global call_to_load_video
+_, container, _ = st.columns([side, 47, side])
+container.video(data=VIDEO_DATA)
+tab1, tab2, tab3, tab4 = st.tabs(["Intro", "Transcription", "Embedding", "Chat with the Video"])
+with tab1:
+    st.markdown("### How does it work?")
+    st.markdown('Read the article to know how it works: [Medium Article]("https://medium.com/@dan.avila7/youtube-gpt-start-a-chat-with-a-video-efe92a499e60")')
+    st.write("Youtube GPT was written with the following tools:")
+    st.markdown("#### Code GPT")
+    st.write("All code was written with the help of Code GPT. Visit [codegpt.co]('https://codegpt.co') to get the extension.")
+    st.markdown("#### Streamlit")
+    st.write("The design was written with [Streamlit]('https://streamlit.io/').")
+    st.markdown("#### Whisper")
+    st.write("Video transcription is done by [OpenAI Whisper]('https://openai.com/blog/whisper/').")
+    st.markdown("#### Embedding")
+    st.write('[Embedding]("https://platform.openai.com/docs/guides/embeddings") is done via the OpenAI API with "text-embedding-ada-002"')
+    st.markdown("#### GPT-3")
+    st.write('The chat uses the OpenAI API with the [GPT-3]("https://platform.openai.com/docs/models/gpt-3") model "text-davinci-003""')
+    st.markdown("""---""")
+    st.write('Author: [Daniel Ávila](https://www.linkedin.com/in/daniel-avila-arias/)')
+    st.write('Repo: [Github](https://github.com/davila7/youtube-gpt)')
+    st.write("This software was developed with Code GPT, for more information visit: https://codegpt.co")
+with tab2: 
+    st.header("Transcription:")
+    if(os.path.exists("youtube_video.mp4")):
+        audio_file = open('youtube_video.mp4', 'rb')
+        audio_bytes = audio_file.read()
+        st.audio(audio_bytes, format='audio/ogg')
+    if os.path.exists("transcription.csv"):
+        df = pd.read_csv('transcription.csv')
+        st.write(df)
+with tab3:
+    st.header("Embedding:")
+    if os.path.exists("word_embeddings.csv"):
+        df = pd.read_csv('word_embeddings.csv')
+        st.write(df)
+with tab4:
+    user_secret = st.text_input(label = ":blue[OpenAI API key]",
+                                placeholder = "Paste your openAI API key, sk-",
+                                type = "password")
+    st.write('To obtain an API Key you must create an OpenAI account at the following link: https://openai.com/api/')
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = []
 
-    if call_to_load_video == 0:
-        st.write('yes')
-        result = process_video(url=url) if url else process_video(video=video)
-        call_to_load_video += 1
+    if 'past' not in st.session_state:
+        st.session_state['past'] = []
 
-    texts, start_time_list = [], []
+    def get_text():
+        if user_secret:
+            st.header("Ask me something about the video:")
+            input_text = st.text_input("You: ","", key="input")
+            return input_text
+    user_input = get_text()
 
-    for res in result['segments']:
-        start = res['start']
-        text = res['text']
+    def get_embedding_text(api_key, prompt):
+        openai.api_key = user_secret
+        response = openai.Embedding.create(
+            input= prompt.strip(),
+            model="text-embedding-ada-002"
+        )
+        q_embedding = response['data'][0]['embedding']
+        df=pd.read_csv('word_embeddings.csv', index_col=0)
+        df['embedding'] = df['embedding'].apply(eval).apply(np.array)
 
-        start_time = dt.datetime.fromtimestamp(start)
-        start_time_formatted = start_time.strftime("%H:%M:%S")
+        df['distances'] = distances_from_embeddings(q_embedding, df['embedding'].values, distance_metric='cosine')
+        returns = []
+        
+        # Sort by distance with 2 hints
+        for i, row in df.sort_values('distances', ascending=True).head(4).iterrows():
+            # Else add it to the text that is being returned
+            returns.append(row["text"])
 
-        texts.append(''.join(text))
-        start_time_list.append(start_time_formatted)
+        # Return the context
+        return "\n\n###\n\n".join(returns)
 
-    texts_with_timestamps = dict(zip(texts, start_time_list))
-    formatted_texts = {
-        text: dt.datetime.strptime(str(timestamp), '%H:%M:%S')
-        for text, timestamp in texts_with_timestamps.items()
-    }
+    def generate_response(api_key, prompt):
+        one_shot_prompt = '''I am YoutubeGPT, a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer.
+        Q: What is human life expectancy in the United States?
+        A: Human life expectancy in the United States is 78 years.
+        Q: '''+prompt+'''
+        A: '''
+        completions = openai.Completion.create(
+            engine = "text-davinci-003",
+            prompt = one_shot_prompt,
+            max_tokens = 1024,
+            n = 1,
+            stop=["Q:"],
+            temperature=0.2,
+        )
+        message = completions.choices[0].text
+        return message
 
-    grouped_texts = []
-    current_group = ''
-    time_list = [list(formatted_texts.values())[0]]
-    previous_time = None
-    time_difference = dt.timedelta(seconds=30)
-
-    for text, timestamp in formatted_texts.items():
-
-        if previous_time is None or timestamp - previous_time <= time_difference:
-            current_group += text
-        else:
-            grouped_texts.append(current_group)
-            time_list.append(timestamp)
-            current_group = text
-        previous_time = time_list[-1]
-
-    # Append the last group of texts
-    if current_group:
-        grouped_texts.append(current_group)
-
-    return grouped_texts, time_list
-
-# Function to get the title of the video (either from URL or uploaded file)
-def get_title(url, video):
-    if url != None:
-        yt = YouTube(url)
-        title = yt.title
-    else:
-        title = os.path.basename(video)
-        title = title[:-4]
-    return title
-
-# Function to create a ConversationalRetrievalChain
-def make_chain(url=None, video=None):
-    global chain, run_once_flag
-
-    if not url and not video:
-        st.error('Please provide a Youtube link or Upload a video')
-    if not run_once_flag:
-        run_once_flag = True
-        title = get_title(url, video).replace(' ','-')
-
-        grouped_texts, time_list = process_text(url=url) if url else process_text(video=video)
-        time_list = [{'source': str(t.time())} for t in time_list]
-
-        vector_stores = Chroma.from_texts(texts=grouped_texts,collection_name= 'test',embedding=OpenAIEmbeddings(), metadatas=time_list)
-        chain = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0.0), 
-                                                    retriever=vector_stores.as_retriever(search_kwargs={"k": 5}),
-                                                    return_source_documents=True )
-
-        return chain
-    else:
-        return chain
-
-# Function to reset app variables
-def reset_vars():
-    global chat_history, result, chain, run_once_flag, call_to_load_video
-
-    os.environ['OPENAI_API_KEY'] = ''
-    chat_history = None
-    result, chain = None, None
-    run_once_flag, call_to_load_video = False, 0
-
-    return [], '', None, None, None
-
-# Main Streamlit app
-def main():
-    global chain, chat_history
-
-    with st.sidebar:
-        #enable_box = st.textbox(label='Upload your OpenAI API key', value=None, key="enable_box")
-        enable_box = st.text_input(label='Upload your OpenAI API key', value=None, key="enable_box")
-        disable_box = st.text_area(label='OpenAI API key is Set', value='OpenAI API key is Set', key="disable_box")
-       # disable_box = st.textbox(label='OpenAI API key is Set', value='OpenAI API key is Set', key="disable_box")
-      #  remove_box = st.textbox(label='Your API key successfully removed', value='Your API key successfully removed', key="remove_box")
-        remove_box = st.text("Your API key successfully removed")
-        pause = st.button(label='Pause', key="pause")
-        resume = st.button(label='Resume', key="resume")
-
-    if st.sidebar.button("Change Key"):
-        set_apikey(enable_box)
-        st.success("OpenAI API key is set.")
-
-    if st.sidebar.button("Remove Key"):
-        remove_key_box()
-        st.success("Your API key has been successfully removed.")
-
-    if st.sidebar.button("Reset App"):
-        chat_history, result, chain, run_once_flag, call_to_load_video = reset_vars()
-        st.success("App has been reset.")
-
-    with st.sidebar:
-        st.write("Please reset the app after being done with the app to remove resources")
-
-    st.sidebar.markdown("---")
-
-    with st.sidebar:
-        st.markdown("### Transcription and Chatbot")
-        st.write("1. Upload a video file (e.g., MP4) or provide a YouTube link.")
-        st.write("2. Click 'Initiate Transcription' to transcribe the video.")
-        st.write("3. Ask questions or have a conversation with the chatbot.")
-        st.write("4. Reset the app to remove resources.")
-
-    st.sidebar.markdown("---")
-
-    st.sidebar.text("Chat History")
-
-    # Main content
-    st.markdown("## Video Transcription and Chatbot")
-
-    st.markdown("### Upload Video or Provide YouTube Link")
-
-    col1, col2 = st.beta_columns(2)
-
-    with col1:
-        video = st.file_uploader("Upload a video file (MP4)", type=["mp4"])
-
-    with col2:
-        yt_link = st.text_input("Paste a YouTube link here")
-
-    st.write("---")
-
-    if st.button("Initiate Transcription"):
-        if video and yt_link:
-            st.error("Upload a video or provide a YouTube link, but not both.")
-        elif not video and not yt_link:
-            st.error("Provide a YouTube link or upload a video.")
-        else:
-            if video:
-                video_path = tempfile.mktemp(suffix=".mp4")
-                with open(video_path, "wb") as f:
-                    f.write(video.read())
-                process_video(video=video_path)
-            else:
-                process_video(url=yt_link)
-
-    st.write("---")
-
-    if result:
-        st.markdown("### Video Transcription")
-
-        texts, time_list = process_text()
-        for text, timestamp in zip(texts, time_list):
-            st.write(f"{timestamp}: {text}")
-
-        st.write("---")
-
-        st.markdown("### Chat with the Chatbot")
-        st.write("Ask questions or have a conversation with the chatbot.")
-
-        chat_input = st.text_input("Enter your question or message")
-        if st.button("Send"):
-            if chat_input:
-                response = chain({"question": chat_input, 'chat_history': chat_history}, return_only_outputs=True)
-                chat_history.append((chat_input, response["answer"]))
-                st.write(f"You: {chat_input}")
-                st.write(f"Chatbot: {response['answer']}")
-            else:
-                st.error("Please enter a question or message.")
-
-if __name__ == "__main__":
-    main()
+    if user_input:
+        text_embedding = get_embedding_text(user_secret, user_input)
+        title = pd.read_csv('transcription.csv')['title']
+        string_title = "\n\n###\n\n".join(title)
+        user_input_embedding = 'Using this context: "'+string_title+'. '+text_embedding+'", answer the following question. \n'+user_input
+        # st.write(user_input_embedding)
+        output = generate_response(user_secret, user_input_embedding)
+        st.session_state.past.append(user_input)
+        st.session_state.generated.append(output)
+    if st.session_state['generated']:
+        for i in range(len(st.session_state['generated'])-1, -1, -1):
+            message(st.session_state["generated"][i], key=str(i))
+            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
