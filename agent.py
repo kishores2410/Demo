0cae77a0e7ae912197cca52274d58f2b5d2c45f7
Kishore S <111968897+kishores2410@users.noreply.github.com>
2023-09-28 17:39:48 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,61 +1,251 @@
 import os
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
-
+import tempfile
+import whisper
+import datetime as dt
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
-from langchain.llms import OpenAI
+from pytube import YouTube
+from typing import TYPE_CHECKING, Any, Generator, List
 
-class Agent:
-    def __init__(self, openai_api_key: str = None) -> None:
-        self.embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+chat_history = []
+result = None
+chain = None
+run_once_flag = False
+call_to_load_video = 0
 
-        self.llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+enable_box = gr.Textbox.update(value=None, placeholder='Upload your OpenAI API key', interactive=True)
+disable_box = gr.Textbox.update(value='OpenAI API key is Set', interactive=False)
+remove_box = gr.Textbox.update(value='Your API key successfully removed', interactive=False)
+pause = gr.Button.update(interactive=False)
+resume = gr.Button.update(interactive=True)
+update_video = gr.Video.update(value=None)
+update_yt = gr.HTML.update(value=None)
 
-        self.chat_history = None
-        self.chain = None
-        self.db = None
-        self.current_pdf = None  # To keep track of the currently ingested PDF
-    
-    def ask(self, question: str) -> str:
-        if self.chain is None:
-            response = "Please, add a document."
+
+def set_apikey(api_key):
+    os.environ['OPENAI_API_KEY'] = api_key
+    return disable_box
+
+
+def enable_api_box():
+    return enable_box
+
+
+def remove_key_box():
+    os.environ['OPENAI_API_KEY'] = ''
+    return remove_box
+
+
+def reset_vars():
+    global chat_history, result, chain, run_once_flag, call_to_load_video
+
+    os.environ['OPENAI_API_KEY'] = ''
+    chat_history = None
+    result, chain = None, None
+    run_once_flag, call_to_load_video = False, 0
+
+    return [], '', gr.Video.update(value=None), gr.HTML.update(value=None)
+
+
+def load_video(url: str) -> str:
+    global result
+
+    yt = YouTube(url)
+    target_dir = os.path.join('/tmp', 'Youtube')
+    if not os.path.exists(target_dir):
+        os.mkdir(target_dir)
+
+    if os.path.exists(target_dir + '/' + yt.title + '.mp4'):
+        return target_dir + '/' + yt.title + '.mp4'
+    try:
+
+        yt.streams.filter(only_audio=True)
+        stream = yt.streams.get_audio_only()
+        print('----DOWNLOADING AUDIO FILE----')
+        stream.download(output_path=target_dir)
+    except:
+        raise gr.Error('Issue in Downloading video')
+
+    return target_dir + '/' + yt.title + '.mp4'
+
+
+def process_video(video=None, url=None) -> dict[str, str | list]:
+
+    if url:
+        file_dir = load_video(url)
+    else:
+        file_dir = video
+
+    print('Transcribing Video with whisper base model')
+    model = whisper.load_model("base")
+    result = model.transcribe(file_dir)
+
+    return result
+
+
+def process_text(video=None, url=None) -> tuple[list, list[dt.datetime]]:
+    global call_to_load_video
+
+    if call_to_load_video == 0:
+        print('yes')
+        result = process_video(url=url) if url else process_video(video=video)
+        call_to_load_video += 1
+
+    texts, start_time_list = [], []
+
+    for res in result['segments']:
+        start = res['start']
+        text = res['text']
+
+        start_time = dt.datetime.fromtimestamp(start)
+        start_time_formatted = start_time.strftime("%H:%M:%S")
+
+        texts.append(''.join(text))
+        start_time_list.append(start_time_formatted)
+
+    texts_with_timestamps = dict(zip(texts, start_time_list))
+    formatted_texts = {
+        text: dt.datetime.strptime(str(timestamp), '%H:%M:%S')
+        for text, timestamp in texts_with_timestamps.items()
+    }
+
+    grouped_texts = []
+    current_group = ''
+    time_list = [list(formatted_texts.values())[0]]
+    previous_time = None
+    time_difference = dt.timedelta(seconds=30)
+
+    for text, timestamp in formatted_texts.items():
+
+        if previous_time is None or timestamp - previous_time <= time_difference:
+            current_group += text
         else:
-            response = self.chain({"question": question, "chat_history": self.chat_history})
-            response = response["answer"].strip()
-    
-            # Check if the response contains "I don't know" and replace it with the desired message
-            if "I don't know." in response:
-                response = "Sorry, I am yet to be trained on this topic. Please try some other question related to the uploaded file."
-            else:
-                # Include the reference to the PDF file if available
-                if self.current_pdf:
-                    response += f" (Ref: {self.current_pdf})"
-                
-            self.chat_history.append((question, response))
-        return response
+            grouped_texts.append(current_group)
+            time_list.append(timestamp)
+            current_group = text
+        previous_time = time_list[-1]
+
+    # Append the last group of texts
+    if current_group:
+        grouped_texts.append(current_group)
+
+    return grouped_texts, time_list
 
 
+def get_title(url, video):
+    if url != None:
+        yt = YouTube(url)
+        title = yt.title
+    else:
+        title = os.path.basename(video)
+        title = title[:-4]
+    return title
 
 
-    def ingest(self, file_path: os.PathLike, pdf_name: str) -> None:
-        loader = PyPDFLoader(file_path)
-        documents = loader.load()
-        splitted_documents = self.text_splitter.split_documents(documents)
-        
-        self.current_pdf = pdf_name  # Store the PDF file name
-        
-        if self.db is None:
-            self.db = FAISS.from_documents(splitted_documents, self.embeddings)
-            self.chain = ConversationalRetrievalChain.from_llm(self.llm, self.db.as_retriever())
-            self.chat_history = []
-        else:
-            self.db.add_documents(splitted_documents)
+def check_path(url=None, video=None):
+    if url:
+        yt = YouTube(url)
+        if os.path.exists('/tmp/Youtube' + yt.title + '.mp4'):
+            return True
+    else:
+        if os.path.exists(video):
+            return True
+    return False
 
-    def forget(self) -> None:
-        self.db = None
-        self.chain = None
-        self.chat_history = None
-        self.current_pdf = None
+
+def make_chain(url=None, video=None) -> (ConversationalRetrievalChain | Any | None):
+    global chain, run_once_flag
+
+    if not url and not video:
+        raise gr.Error('Please provide a Youtube link or Upload a video')
+    if not run_once_flag:
+        run_once_flag = True
+        title = get_title(url, video).replace(' ', '-')
+
+        # if not check_path(url, video):
+        grouped_texts, time_list = process_text(url=url) if url else process_text(video=video)
+        time_list = [{'source': str(t.time())} for t in time_list]
+
+        vector_stores = Chroma.from_texts(texts=grouped_texts, collection_name='test', embedding=OpenAIEmbeddings(),
+                                          metadatas=time_list)
+        chain = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0.0),
+                                                      retriever=vector_stores.as_retriever(search_kwargs={"k": 5}),
+                                                      return_source_documents=True)
+
+        return chain
+    else:
+        return chain
+
+
+def QuestionAnswer(history, query=None, url=None, video=None) -> Generator[Any | None, Any, None]:
+    global chat_history, chain
+
+    if video and url:
+        raise gr.Error('Upload a video or a Youtube link, not both')
+    elif not url and not video:
+        raise gr.Error('Provide a Youtube link or Upload a video')
+
+    result = chain({"question": query, 'chat_history': chat_history}, return_only_outputs=True)
+    chat_history += [(query, result["answer"])]
+    for char in result['answer']:
+        history[-1][-1] += char
+        yield history, ''
+
+
+def add_text(history, text):
+    if not text:
+        raise gr.Error('enter text')
+    history = history + [(text, '')]
+    return history
+
+
+def embed_yt(yt_link: str):
+    # This function embeds a YouTube video into the page.
+
+    # Check if the YouTube link is valid.
+    if not yt_link:
+        raise gr.Error('Paste a Youtube link')
+
+    # Set the global variable `run_once_flag` to False.
+    # This is used to prevent the function from being called more than once.
+    run_once_flag = False
+
+    # Set the global variable `call_to_load_video` to 0.
+    # This is used to keep track of how many times the function has been called.
+    call_to_load_video = 0
+
+    # Create a chain using the YouTube link.
+    make_chain(url=yt_link)
+
+    # Get the URL of the YouTube video.
+    url = yt_link.replace('watch?v=', '/embed/')
+
+    # Create the HTML code for the embedded YouTube video.
+    embed_html = f"""<iframe width="750" height="315" src="{url}"
+                     title="YouTube video player" frameborder="0"
+                     allow="accelerometer; autoplay; clipboard-write;
+                     encrypted-media; gyroscope; picture-in-picture"
+                     allowfullscreen></iframe>"""
+
+    # Return the HTML code and an empty list.
+    return embed_html, []
+
+
+def embed_video(video=str | None):
+    # This function embeds a video into the page.
+
+    # Check if the video is valid.
+    if not video:
+        raise gr.Error('Upload a Video')
+
+    # Set the global variable `run_once_flag` to False.
+    # This is used to prevent the function from being called more than once.
+    run_once_flag = False
+
+    # Create a chain using the video.
+    make_chain(video=video)
+
+    # Return the video and an empty list.
+    return video, []
